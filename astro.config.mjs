dc8a7290b9195d0857c6dd2b755764de6f15a333
JebGy <martinmunayco123@hotmail.com>
2026-06-16 11:17:22 -0500

    FEAT: MVP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://favio.munayco.dev',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [sitemap()]
+});
